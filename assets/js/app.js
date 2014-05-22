4b1385e7889b35fa3ca072adda1f00fa8245c8fe
RichMartel <richmartel@gmail.com>
2014-05-22 14:17:36 -0400

    notifications are working

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,15 +17,17 @@ angular
 			.otherwise({
 				redirectTo: '/',
 			});
-		growlProvider.globalTimeToLive({success: 2000, info: 3000, warning: 5000, error: 10000});
+		growlProvider.globalTimeToLive({success: 4000, info: 6000, warning: 10000, error: 20000});
 	}])
 	.controller('AppController', ['$scope', 'growl', function($scope, growl) {
 		'use strict';
 		$scope.ui = {
 			sidenavActive: false,
 		};
-		growl.success('success notification');
-		growl.info('info notification');
-		growl.warning('warning notification');
-		growl.error('error notification');
+		$scope.addNotifications = function() {
+			growl.success('success notification');
+			growl.info('info notification');
+			growl.warning('warning notification');
+			growl.error('error notification');
+		}
 	}]);

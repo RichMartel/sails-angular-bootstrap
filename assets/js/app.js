f848e3c9ea5cae608be087de9cf452c8029b528a
RichMartel <richmartel@gmail.com>
2014-05-23 19:22:44 -0400

    client to server logging boilerplate

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,25 @@ angular
 	.module('app', [
 		'ngRoute',
 		'ngAnimate',
+		'ngResource',
 		'ngSails',
 		'angular-growl',
 		'app.home',
 	])
-	.config(['$routeProvider', '$locationProvider', '$provide', 'growlProvider', function($routeProvider, $locationProvider, $provide, growlProvider) {
+	.config(['$provide', '$locationProvider', '$routeProvider', 'growlProvider', function($provide, $locationProvider, $routeProvider, growlProvider) {
 		'use strict';
+		$provide.decorator('$log', ['$delegate', 'logHttpService', function($delegate, logHttpService) {
+			var logError = $delegate.error;
+			var logWarn = $delegate.warn;
+			$delegate.error = function(message) {
+				logError(message);
+				alert(message);
+			}
+			$delegate.warn = function(message) {
+				logWarn(message);
+			}
+			return $delegate;
+		}]);
 		//$locationProvider.html5Mode(true);
 		$routeProvider
 			.when('/', {
@@ -17,32 +30,42 @@ angular
 			.otherwise({
 				redirectTo: '/',
 			});
-		$provide.decorator('$exceptionHandler', function($delegate) {
-			return function(exception, cause) {
-				$delegate(exception, cause);
-				try {
-					$http.post('/clientlogger', angular.toJson({
-						url: $window.location.href,
-						message: exception.toString(),
-						stacktrace: printStackTrace({e: exception}),
-						cause: (cause || ''),
-					}))
-					.failure(function(data, status) {
-						$log.error('Server responded with an error while pushing client error to server:', status, data);
-					});
-				}
-				catch(e) {
-					$log.error('Failed to push client error to server:', e);
-				}
-			};
-		});
 		growlProvider.globalTimeToLive({success: 5000, info: 5000, warning: 10000, error: 10000});
+	}])
+	.factory('logHttpService', ['$window', function($window) {
+		function postLog(exception, cause) {
+			try {
+				var httpReq = new XMLHttpRequest();
+				var sendData = {
+					url: $window.location.href,
+					message: exception.toString(),
+					stacktrace: printStackTrace({e: exception}),
+					cause: (cause || ''),
+				}
+				httpReq.onreadystatechange = function() {
+					if (httpReq.readyState === 4) {
+						if (httpReq.status === 200) {
+							return console.info(httpReq.status, httpReq.responseText);
+						}
+						else {
+							return console.error(httpReq.status, httpReq.responseText);
+						}
+					}
+				}
+				httpReq.open('POST', '/clientlogger', true);
+				httpReq.send(sendData);
+			}
+			catch (e) {
+				console.error(e)
+			}
+		}
+		return postLog;
 	}])
 	.controller('AppController', ['$scope', 'growl', function($scope, growl) {
 		'use strict';
 		$scope.ui = {
 			sidenavActive: false,
-		};
+		}
 		$scope.addNotification = function() {
 			switch (Math.floor(Math.random() * 4)) {
 				case 0:
@@ -58,4 +81,12 @@ angular
 					growl.error('No data found', {title: 'FAILURE:'});
 			}
 		}
+		// setTimeout(function() {
+		// 	try {
+		// 		throw new Error('Test error handler');
+		// 	}
+		// 	catch (e) {
+		// 		alert(e.name + ': ' + e.message);
+		// 	}
+		// }, 2000)
 	}])
